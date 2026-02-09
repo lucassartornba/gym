775b8f2f826a3f50d80627813a7cf2589069eb5d
lucassartornba <prazereusouodoutor@gmail.com>
2026-02-09 16:21:30 -0300

    vite.config.ts
    import { defineConfig } from 'vite'
    import react from '@vitejs/plugin-react'
    
    export default defineConfig({
      plugins: [react()],
      base: '/',
    })

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+})
+
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
